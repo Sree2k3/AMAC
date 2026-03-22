dc8bf0e0c8a3c53f9eb65317ba649f5a2d59e30c
Sreekant <1408sreekant@gmail.com>
2026-03-22 21:38:15 +0530

    New start of repo

--- backend/app/schemas/request.py
+++
@@ -1,6 +0,0 @@
-﻿from pydantic import BaseModel, Field
-
-class AskRequest(BaseModel):
-    repo: str = Field(..., description="GitHub repo URL")
-    question: str = Field(..., description="User's natural-language question")
-
